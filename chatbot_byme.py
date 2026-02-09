890a6644ce0b9a2aff1c7b4917224e39d6a553e8
YabaCoffie <coffieyabateresa@gmail.com>
2026-02-09 14:54:30 +0100

    nouveau changement

--- chatbot_byme.py
+++ chatbot_byme.py
@@ -25,21 +25,23 @@ for message in st.session_state.chat_history :
 
 user_prompt = st.chat_input("Quel modèle souhaitez vous utiliser ? ")
 
-if user_prompt.lower() == "groq":
-    llm = ChatGroq(
-        model = "llama-3.3-70b-versatile",
-        temperature = 0.0,
-    )
-elif user_prompt.lower() == "gemini":
-    llm = ChatGoogleGenerativeAI(
-        model = 'gemini-2.5-flash-lite',
-        temperature = 0.0,
-    )
-elif user_prompt.lower() == "openai":
-    llm = ChatOpenAI(
-        model = "gpt-3.5-turbo",
-        temperature = 0.0,
-    )
+if user_prompt is not None :
+    if user_prompt.lower() == "groq":
+        llm = ChatGroq(
+            model = "llama-3.3-70b-versatile",
+            temperature = 0.0,
+        )
+    elif user_prompt.lower() == "gemini":
+        llm = ChatGoogleGenerativeAI(
+            model = 'gemini-2.5-flash-lite',
+            temperature = 0.0,
+        )
+    else:
+        llm = ChatOpenAI(
+            model = "gpt-3.5-turbo",
+            temperature = 0.0,
+        )
+
 
 if user_prompt : 
     st.chat_message("user").markdown(user_prompt)
