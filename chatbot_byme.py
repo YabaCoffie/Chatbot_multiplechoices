74f0e15d88cd4eff66409fbd7a29022fe29c6ea0
YabaCoffie <coffieyabateresa@gmail.com>
2026-02-09 13:05:19 +0100

    nouveau changement

--- chatbot_byme.py
+++ chatbot_byme.py
@@ -25,5 +25,33 @@ for message in st.session_state.chat_history :
 
 user_prompt = st.chat_input("Quel modèle souhaitez vous utiliser ? ")
 
-
+if user_prompt : 
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user","content":user_prompt})
     
+    if user_prompt.lower().split() == "groq":
+        llm = ChatGroq(
+            model = "llama-3.3-70b-versatile",
+            temperature = 0.0,
+        )
+    elif user_prompt.lower().split() == "genai":
+        llm = ChatGroq(
+            model = 'gemini-2.5-flash-lite',
+            temperature = 0.0,
+        )
+    elif user_prompt.lower().split() == "openai":
+        llm = ChatGroq(
+            model = "gpt-3.5-turbo",
+            temperature = 0.0,
+        )
+    
+    response = llm.invoke(
+        input({"role":"system", "content" : "You are a helpful assistant"})
+    )
+    
+    assistant_response = response.content
+    
+    st.session_state.chat_history.append({"role":"system","content": assistant_response})
+    
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
